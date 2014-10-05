00a222f73e3c59a2b210daa865262038f3084b05
Chet Kener <Cl3Kener@gmail.com>
2014-10-05 18:23:24 -0400

    Revert "Change Mounting Options for F2FS flexibility"
    This reverts commit 54c7fc9387c18316c5c2e65c79667c41cf2c09b7.

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -152,11 +152,19 @@ class EdifyGenerator(object):
 
   def Mount(self, mount_point):
     """Mount the partition with the given mount_point."""
-    self.script.append('run_program("/sbin/busybox", "mount", "/system");')
+    fstab = self.info.get("fstab", None)
+    if fstab:
+      p = fstab[mount_point]
+      self.script.append('mount("%s", "%s", "%s", "%s");' %
+                         (p.fs_type, common.PARTITION_TYPES[p.fs_type],
+                          p.device, p.mount_point))
+      self.mounts.add(p.mount_point)
 
   def Unmount(self, mount_point):
     """Unmount the partiiton with the given mount_point."""
-    self.script.append('unmount("/system");')
+    if mount_point in self.mounts:
+      self.mounts.remove(mount_point)
+      self.script.append('unmount("%s");' % (mount_point,))
 
   def UnpackPackageDir(self, src, dst):
     """Unpack a given directory from the OTA package into the given
@@ -177,9 +185,14 @@ class EdifyGenerator(object):
   def FormatPartition(self, partition):
     """Format the given partition, specified by its mount point (eg,
     "/system")."""
-    self.script.append('package_extract_file("system/extras/format-system.sh", "/tmp/format-system.sh");')
-    self.script.append('set_perm(0, 0, 0777, "/tmp/format-system.sh");')
-    self.script.append('run_program("/tmp/format-system.sh");')
+
+    reserve_size = 0
+    fstab = self.info.get("fstab", None)
+    if fstab:
+      p = fstab[partition]
+      self.script.append('format("%s", "%s", "%s", "%s", "%s");' %
+                         (p.fs_type, common.PARTITION_TYPES[p.fs_type],
+                          p.device, p.length, p.mount_point))
 
   def DeleteFiles(self, file_list):
     """Delete all files in file_list."""
